b8bc41bb42632f1cd1020dd7765048fed11ca04f
Tasawar Memon <tasawar.memon08@gmail.com>
2026-08-03 16:53:24 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -38,6 +38,9 @@ self.addEventListener("fetch", (event) => {
       if (cached) return cached;
       return fetch(event.request)
         .then((response) => {
+          if (!response || response.status !== 200 || response.type !== "basic") {
+            return response;
+          }
           const clone = response.clone();
           caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
           return response;
